da7353225d66a572b734ff97f6329a7dffbb30e3
alston <alston.chan22@sitechhs.com>
2023-11-02 16:51:50 -0400

    blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
-import { Badge } from "@/components/ui/badge"
-import Image from "next/image"
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
 import adastra from "@/public/adastra.png";
-import dbk from "@/public/dbk.png"
+import dbk from "@/public/dbk.png";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
 	return (
@@ -66,13 +74,13 @@ export default function Home() {
 			</div>
 
 			<section className="text-white">
-				<div className="max-w-3xl">
-					<h2 className="text-3xl font-bold sm:text-4xl">Projects</h2>
+				<div className="">
+					<h2 className="text-3xl font-bold sm:text-4xl">projects</h2>
 				</div>
 
 				<div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-					<div className="relative h-40 overflow-hidden sm:h-80 lg:h-full">
-            <Image
+					<div className="relative h-96 overflow-hidden sm:h-80 lg:h-full">
+						<Image
 							alt="dbk"
 							src={dbk}
 							className="absolute inset-0 h-full w-full object-top object-cover"
@@ -80,34 +88,42 @@ export default function Home() {
 					</div>
 
 					<div className="lg:py-16">
-            <h3 className="text-white text-3xl font-semibold mb-4">
+						<h3 className="text-white text-3xl font-semibold mb-4">
 							DBK Stylebook
 						</h3>
 						<article className="space-y-4">
-              <div className="flow-root">
-                <ul className="flex flex-wrap space-x-2">
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">Next.js</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">TypeScript</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">Supabase</Badge>
-                  </li>
-                </ul>
-              </div>
+							<div className="flow-root">
+								<ul className="flex flex-wrap space-x-2">
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											Next.js
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											TypeScript
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											Supabase
+										</Badge>
+									</li>
+								</ul>
+							</div>
 
 							<p className="text-gray-500">
-                built an interactive web app for the diamondback newspaper's stylebook, which was previously a 50 page pdf file. integrated a supabase backend to allow edits to be made to the stylebook.
+								built an interactive web app for the diamondback
+								newspaper's stylebook, which was previously a 50
+								page pdf file. integrated a supabase backend to
+								allow edits to be made to the stylebook.
 							</p>
 						</article>
-            
 					</div>
 				</div>
 
-        <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-					<div className="relative h-40 overflow-hidden sm:h-80 lg:h-full">
+				<div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+					<div className="relative h-96 overflow-hidden sm:h-80 lg:h-full">
 						<Image
 							alt="ad astra"
 							src={adastra}
@@ -116,39 +132,76 @@ export default function Home() {
 					</div>
 
 					<div className="lg:py-16">
-            <h3 className="text-white text-3xl font-semibold mb-4">
+						<h3 className="text-white text-3xl font-semibold mb-4">
 							Ad Astra AI
 						</h3>
 						<article className="space-y-4">
-              <div className="flow-root">
-                <ul className="flex flex-wrap gap-2">
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">Next.js</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">TypeScript</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">Supabase</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">OpenAI</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">Langchain</Badge>
-                  </li>
-                  <li className=" ">
-                    <Badge className="bg-white text-slate-800">Pinecone</Badge>
-                  </li>
-                </ul>
-              </div>
+							<div className="flow-root">
+								<ul className="flex flex-wrap gap-2">
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											Next.js
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											TypeScript
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											Supabase
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											OpenAI
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											Langchain
+										</Badge>
+									</li>
+									<li className=" ">
+										<Badge className="bg-white text-slate-800">
+											Pinecone
+										</Badge>
+									</li>
+								</ul>
+							</div>
 
 							<p className="text-gray-500">
-								used openai's gpt-3.5 to create a teacher's assistant for my computer science classes, particularly cmsc216 introduction to computer systems. trained on 100+ pages of cmsc216 notes.
+								used openai's gpt-3.5 to create a teacher's
+								assistant for my computer science classes,
+								particularly cmsc216 introduction to computer
+								systems. trained on 100+ pages of cmsc216 notes.
 							</p>
 						</article>
-            
 					</div>
+				</div>
+			</section>
+
+      <section className="text-white mt-8">
+				<div className="max-w-3xl">
+					<h2 className="text-3xl font-bold sm:text-4xl">gallery</h2>
+				</div>
+      </section>
+
+			<section className="text-white mt-8">
+				<div className="max-w-3xl">
+					<h2 className="text-3xl font-bold sm:text-4xl">blog</h2>
+				</div>
+				<div className="mt-8">
+					<Card className="bg-zinc-800 text-white">
+						<CardHeader>
+							<CardTitle>my first post!</CardTitle>
+							<CardDescription className="text-zinc-200">oct. 30, 2023</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<p>this is where i would put my blog content, if I had any!</p>
+						</CardContent>
+					</Card>
 				</div>
 			</section>
 		</div>
